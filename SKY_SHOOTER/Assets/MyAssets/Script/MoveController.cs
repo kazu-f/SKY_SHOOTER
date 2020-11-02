4ee176e5543d8c0e011a6b7addd6e3ec74de1daf
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-11-02 11:18:34 +0900

    プレイヤーの移動処理を簡易的に実装

---
+++ SKY_SHOOTER/Assets/MyAssets/Script/MoveController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float MOVE_SPEED;
+    public float MOVE_Z;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterMove();
+
+    }
+
+    void CharacterMove()
+    {
+        Vector3 vMove = new Vector3(0, 0, 0);
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            vMove.x = -MOVE_SPEED;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            vMove.x = MOVE_SPEED;
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            vMove.y = MOVE_SPEED;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            vMove.y = -MOVE_SPEED;
+        }
+
+        Rigidbody rigi = GetComponent<Rigidbody>();
+        rigi.velocity = vMove;
+    }
+}
