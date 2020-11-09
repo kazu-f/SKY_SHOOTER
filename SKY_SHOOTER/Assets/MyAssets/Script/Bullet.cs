fd9da1562a4d2108d01bda7e86d91f48cd16feb1
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-11-09 12:14:48 +0900

    プレイヤーの弾を打ち出す処理を追加。
    １．Bulletモデルの作成
    ２．弾の移動処理スクリプト作成。
    ３．Playerが弾を打ち出す処理のスクリプト作成。

---
+++ SKY_SHOOTER/Assets/MyAssets/Script/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float MOVE_SPEED;
+    Vector3 moveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //moveDir = new Vector3(0.0f,0.0f,1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(
+            moveDir.x * MOVE_SPEED,
+            moveDir.y * MOVE_SPEED,
+            moveDir.z * MOVE_SPEED
+            );
+    }
+
+    public void SetDirection(Vector3 vDir)
+    {
+        moveDir = vDir;
+    }
+}
---
+++ SKY_SHOOTER/Assets/MyAssets/Script/ShotControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotControl : MonoBehaviour
+{
+    public GameObject BulletObject;
+    public float ShotInterval = 2.0f;
+    float currentInterval = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CountInterval()
+            && Input.GetKey(KeyCode.Z))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        GameObject bullet = Instantiate(
+            BulletObject,
+            transform.position,
+            Quaternion.identity
+            );
+
+        var bulletComp = bullet.GetComponent<Bullet>();
+        Vector3 vDir = new Vector3(0.0f, 0.0f, 1.0f);
+        bulletComp.SetDirection(vDir);
+
+        currentInterval = ShotInterval;
+    }
+
+    bool CountInterval()
+    {
+        currentInterval -= Time.deltaTime;
+        if(currentInterval < 0.0f)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
