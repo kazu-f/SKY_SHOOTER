83291a459849f20bb0fc9bbdaf63ebf98d25fdd9
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-11-30 12:30:06 +0900

    Enemyのあれこれ追加

---
+++ SKY_SHOOTER/Assets/MyAssets/Script/EnemyHP.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour
+{
+    public int m_HP = 3;
+
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+
+    void DeathEnemy()
+    {
+        Destroy(gameObject);
+    }
+
+    bool IsDead()
+    {
+        return m_HP <= 0;
+    }
+}
