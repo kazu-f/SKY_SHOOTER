2924d64505f8e4bbcfd3d1b7e83abd644d5f7395
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-12-07 12:32:42 +0900

    プレイヤーの弾が敵に当たるようになった。
    敵が消える処理まで追加できた。

--- SKY_SHOOTER/Assets/MyAssets/Script/Enemy/EnemyHP.cs
+++ SKY_SHOOTER/Assets/MyAssets/Script/Enemy/EnemyHP.cs
@@ -20,13 +20,29 @@ public class EnemyHP : MonoBehaviour
         
     }
 
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PBullet")
+        {
+            //HPを減らす。
+            m_HP--;
 
+            //当たった弾を消しておく。
+            Destroy(other.gameObject);
+
+            //HPが0になったか？
+            if (IsDead())
+            {
+                DeathEnemy();
+            }
+        }
+    }
+    //Enemyを消す処理。
     void DeathEnemy()
     {
         Destroy(gameObject);
     }
-
+    //HPが0以下になったかどうか。
     bool IsDead()
     {
         return m_HP <= 0;
