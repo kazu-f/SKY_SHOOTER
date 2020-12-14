398f15affbde2368d7748f799c581eea0e2c01d8
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2020-12-14 12:32:03 +0900

    ロックオンのスクリプトの追加だけ

---
+++ SKY_SHOOTER/Assets/MyAssets/Script/Player/LockOnTarget.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOnTarget : MonoBehaviour
+{
+
+
+    private Vector3 oldPos;
+    private GameObject TargetObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
