169090f344bee527475046befe62102e6985c055
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-02-08 12:33:52 +0900

    プレイヤーの移動を多少変更

--- SKY_SHOOTER/Assets/MyAssets/Script/Player/MoveController.cs
+++ SKY_SHOOTER/Assets/MyAssets/Script/Player/MoveController.cs
@@ -7,6 +7,8 @@ public class MoveController : MonoBehaviour
 {
     public float MOVE_SPEED = 50.0f;
     public float MOVE_Z;
+    public Vector2 MOVE_MAXPOS;
+    public Vector2 MOVE_MINPOS;
 
 
     // Start is called before the first frame update
@@ -42,11 +44,24 @@ public class MoveController : MonoBehaviour
         {
             vMove.y = MOVE_SPEED;
         }
+        vMove.z = MOVE_Z;
 
         //vMove.x = MOVE_SPEED * CrossPlatformInputManager.GetAxis("Horizontal");
         //vMove.y = MOVE_SPEED * CrossPlatformInputManager.GetAxis("Vertical");
 
         Rigidbody rigi = GetComponent<Rigidbody>();
         rigi.velocity = vMove;
+
+        MoveLimits();
+    }
+
+    void MoveLimits()
+    {
+        Vector3 newPos;
+        newPos.x = Mathf.Clamp(transform.position.x, MOVE_MINPOS.x, MOVE_MAXPOS.x);
+        newPos.y = Mathf.Clamp(transform.position.y, MOVE_MINPOS.y, MOVE_MAXPOS.y);
+        newPos.z = transform.position.z;
+
+        transform.position = newPos;
     }
 }
