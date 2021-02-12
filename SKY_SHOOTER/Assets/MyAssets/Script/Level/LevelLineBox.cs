8952d479d154c66b96a8bfd0c906eb21d1a67a3b
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-02-12 11:18:23 +0900

    背景モデルの改造

---
+++ SKY_SHOOTER/Assets/MyAssets/Script/Level/LevelLineBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelLineBox : MonoBehaviour
+{
+    public Vector3 BoxSize;
+    public Color BoxColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //ステージエディット中のためにシーンにギズモを表示
+    void OnDrawGizmos()
+    {
+        Gizmos.color = BoxColor;
+        //箱を描画する。
+        Gizmos.DrawWireCube(transform.position, BoxSize);
+    }
+}
