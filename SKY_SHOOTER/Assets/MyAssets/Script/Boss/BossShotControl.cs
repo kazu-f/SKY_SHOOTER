3fb31f66cd5de3155c0d7b4335c038faec44b3f3
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-02-14 23:35:50 +0900

    ボスの弾を散弾にした

--- SKY_SHOOTER/Assets/MyAssets/Script/Boss/BossShotControl.cs
+++ SKY_SHOOTER/Assets/MyAssets/Script/Boss/BossShotControl.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BossShotControl : ShotControl
 {
     public Transform[] SHOT_POINT;
+    public float[] ShotDegX;
+    public float[] ShotDegY;
     private Transform playerTransform;
     bool isShot = false;
     // Start is called before the first frame update
@@ -32,22 +34,40 @@ public class BossShotControl : ShotControl
         //Vector3 vDir = playerTransform.position - SHOT_POINT[0].position;
         //vDir.Normalize();
 
+        Vector3 vDir = playerTransform.position - SHOT_POINT[0].position;
         //全ての発射点から撃つ。
         foreach (var trans in SHOT_POINT)
         {
-            GameObject bullet = Instantiate(
-                BulletObject,
-                trans.position,
-                Quaternion.identity
-                );
-
-            Vector3 vDir = playerTransform.position - trans.position;
             vDir.Normalize();
-            var bulletComp = bullet.GetComponent<Bullet>();
-            bulletComp.SetDirection(vDir);
+            for(int i = 0;i<ShotDegX.Length;i++)
+            {
+                for(int j = 0;j<ShotDegY.Length;j++)
+                {
+                    GenerateShot(trans.position, vDir, ShotDegX[i], ShotDegY[j]);
+                }
+            }
         }
 
         currentInterval = ShotInterval;
+    }
+    //角度をつけてショットする。
+    void GenerateShot(Vector3 pos,Vector3 baseDir,float degX,float degY)
+    {
+        GameObject bullet = Instantiate(
+            BulletObject,
+            pos,
+            Quaternion.identity
+            );
+        //角度をつける。
+        Vector3 result = Quaternion.Euler(degY, degX, 0) * baseDir;
+        result.Normalize();
+
+        Quaternion bulletRot = new Quaternion();
+        bulletRot.SetLookRotation(result);
+        bullet.transform.rotation = bulletRot;
+
+        var bulletComp = bullet.GetComponent<Bullet>();
+        bulletComp.SetDirection(result);
     }
 
     public void SetShotFlag(bool flag)
