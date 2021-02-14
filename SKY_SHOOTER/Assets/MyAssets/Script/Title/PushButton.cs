892a55e5d370aa281e3b46fc865d459f9a6ea8fd
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-02-15 04:14:41 +0900

    Titleを追加する

---
+++ SKY_SHOOTER/Assets/MyAssets/Script/Title/PushButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PushButton : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CrossPlatformInputManager.GetButton("Fire1"))
+        {
+            button.onClick.Invoke();
+        }
+    }
+}
